22c56f21ecb92baa2cb16ba1ea9fe03c337a7d91
Banu balta <nbanu.balta@gmail.com>
2024-01-10 21:27:14 +0300

    Initial commit

---
+++ src/main/java/com/banubalta/AppMain.java
@@ -0,0 +1,33 @@
+package com.banubalta;
+
+import java.util.Scanner;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+/*
+        Kullanicidan satir sayisini alip
+        asagidaki sekli cizdirin
+
+            1
+            1 2
+            1 2 3
+            1 2 3 4
+            1 2 3 4 5
+         */
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen satir sayisini giriniz");
+        int satir = scanner.nextInt();
+
+        for (int i = 1; i <=satir ; i++) { // satir sayisini kontrol eder
+            for (int j = 1; j <=i ; j++) { // sutun sayisi da satir'a bagli
+                System.out.print(j + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
